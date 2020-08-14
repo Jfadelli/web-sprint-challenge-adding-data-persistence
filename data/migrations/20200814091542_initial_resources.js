863a2430fd55b3627d972b1497bbd47f53b95b10
Jason <47641725+Jfadelli@users.noreply.github.com>
2020-08-14 16:35:10 -0700

    'mvpStart'

--- data/migrations/20200814091542_initial_resources.js
+++ data/migrations/20200814091542_initial_resources.js
@@ -2,28 +2,35 @@
 exports.up = function(knex) {
     return knex.schema
     .createTable ('projects', tbl => { 
-        tbl.increments('id');
-        tbl.string('name').notNullable();
-        tbl.string('description');
-        tble.boolean('completed').defaultTo(false);
+        tbl.increments('p_id');
+        tbl.string('p_name').notNullable();
+        tbl.string('p_escription');
+        tbl.boolean('p_completed').notNullable().defaultTo(false);
     })
     
     .createTable ('resources', tbl => { 
-        tbl.increments('id');
-        tbl.string('name').notNullable();
-        tbl.string('description');
+        tbl.increments('r_id');
+        tbl.string('r_name').notNullable();
+        tbl.string('r_description');
     })
 
     .createTable ('tasks', tbl => { 
-        tbl.increments('id');
-        tbl.string('description').notNullable();
-        tbl.string('notes');
-        tble.boolean('completed').defaultTo(0);
+        tbl.increments('t_id');
+        tbl.string('t_description').notNullable();
+        tbl.string('t_notes');
+        tbl.boolean('t_completed').notNullable().defaultTo(false);
+    })
+    
+    .createTable ('project_resources', tbl => {
+        tbl.increments()
+        tbl.integer('project_resources_id').unsigned().notNullable().references('p_id').inTable('projects')
+        tbl.integer('resource_id').unsigned().notNullable().references('r_id').inTable('resources')
     })
 };
 
 exports.down = function(knex) {
     return knex.schema
+        .dropTableIfExists('project_resources')
         .dropTableIfExists('tasks')
         .dropTableIfExists('resources')
         .dropTableIfExists('projects')
